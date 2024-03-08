f1f521e837e1df5616ce9b0b3a6d3cbcbdfe298a
pseudoDjnn <alexander.havers13@gmail.com>
2024-03-08 11:14:41 -0500

    updated rotation and fixed some code logic

--- src/script.js
+++ src/script.js
@@ -18,28 +18,45 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-// const gradientTexture = textureLoader.load('textures/gradients/5.jpg')
-// gradientTexture.magFilter = THREE.NearestFilter
+const particleTexture = textureLoader.load('/textures/gradients/3.jpg')
+
 
 /**
  * Particles test idea
  */
 const parameters = {
-    count: 1000,
-    size: 0.01,
+    count: 10000,
+    size: 0.001,
 }
 
 let geometry = null
 let positions = null
 let points = null
 let fibbonacci = null
-
-// Array to iterate over for particles
-const iterableParticles = [points]
+let colors = null
 
 const generateParticles = () => {
+
     geometry = new THREE.BufferGeometry()
     positions = new Float32Array(parameters.count * 3)
+    colors = new Float32Array(parameters.count * 3)
+
+
+    const material = new THREE.PointsMaterial({
+        color: new THREE.Color('#ffeded'),
+        size: parameters.size,
+        sizeAttenuation: true,
+        transparent: true,
+        alphaMap: particleTexture,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        vertexColors: true
+    })
+
+    points = new THREE.Points(geometry, material)
+    points.position.x = -1.8
+    // points.rotation.y = deltaTime
+
     // Test fibbonacci sequence instead of using Math.random()
     fibbonacci = (i, count = {}) => {
         const i3 = i * 3
@@ -47,36 +64,21 @@ const generateParticles = () => {
         if (i <= 2) return 1;
 
         count[i] = fibbonacci(i - 1, count) + fibbonacci(i - 2, count)
-        positions[i3] = (Math.random() - 0.5) * 3
-        positions[i3 + 1] = (Math.random() - 0.5) * 3
-        positions[i3 + 2] = (Math.random() - 0.5) * 3
+        positions[i3] = (Math.random() - 0.5) * 4
+        positions[i3 + 1] = (Math.random() - 0.5) * 4
+        positions[i3 + 2] = (Math.random() - 0.5) * 4
+        colors[i] = Math.random()
 
         return count[i3]
     }
-
+    fibbonacci(5000)
 
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-
-
-    const material = new THREE.PointsMaterial({
-        color: '#ffeded',
-        size: parameters.size,
-        sizeAttenuation: true,
-        depthWrite: false,
-        blending: THREE.AdditiveBlending
-    })
-    points = new THREE.Points(geometry, material)
-    points.position.x = -1
-
-    fibbonacci(500)
-    // iterableParticles.push({
-    //     mesh: material,
-    //     body: positions
-    // })
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
 
     scene.add(points)
 }
-// geometry.position.y = -0.3
+
 generateParticles()
 
 const directionalLight = new THREE.DirectionalLight('#ffeded', 3)
@@ -111,7 +113,7 @@ window.addEventListener('resize', () => {
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(135, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
 
@@ -141,17 +143,10 @@ const tick = () => {
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
 
-    //Animate particles (keep it simple for now)
-    // generateParticles(Math.sin((deltaTime - 0.5 / points) * 5))
-
-    for (const points of iterableParticles) {
-        // object.
-        // const i3 = i * 3
-        // points.
-        // object += i3
-    }
-
-    // points.attributes.position.needsUpdate = true
+    // Update particles (keeping this simple for now)
+    points.rotation.x = Math.cos(-elapsedTime * 0.0021) * 3
+    points.rotation.y = -(deltaTime - 0.5) * 3
+    points.rotation.z = Math.sin(elapsedTime * 0.0055) * 3
 
     // Update controls
     controls.update()

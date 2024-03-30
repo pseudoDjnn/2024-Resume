57ce130d975df65ca1a867c71ec48cc10cf72060
p5eudoDjnn <alexander.havers@gmail.com>
2024-03-30 11:34:08 -0400

    added particles without movment to the scene

--- src/shaders/WebGLCanvas/Camera.js
+++ src/shaders/WebGLCanvas/Camera.js
@@ -16,7 +16,7 @@ export default class Camera {
   }
 
   setInstance() {
-    this.instance = new THREE.PerspectiveCamera(75, this.sizes.width / this.sizes.height, 0.1, 100)
+    this.instance = new THREE.PerspectiveCamera(34, this.sizes.width / this.sizes.height, 0.1, 100)
     this.instance.position.set(8.5, 5, 13)
     this.scene.add(this.instance)
   }
--- src/shaders/WebGLCanvas/World/World.js
+++ src/shaders/WebGLCanvas/World/World.js
@@ -1,9 +1,14 @@
 import * as THREE from 'three'
 import WebGLCanvas from "../WebGLCanvas";
+import Sizes from '../Utils/Sizes';
+import Environment from './Environment';
+import vertexParticleShader from '../../particles/vertexParticles.glsl'
+import fragmentParticleShader from '../../particles/fragmentParticles.glsl'
 
 export default class World {
   constructor() {
     this.webglCanvas = new WebGLCanvas()
+    this.sizes - new Sizes()
     this.scene = this.webglCanvas.scene
 
     // console.log(this.scene)
@@ -11,8 +16,68 @@ export default class World {
     // Test mesh
     const testMesh = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial({ wireframe: true })
+      new THREE.MeshBasicMaterial()
     )
-    this.scene.add(testMesh)
+    // this.scene.add(testMesh)
+    // console.log(testMesh)
+
+    /**
+     * Particle animation
+     */
+
+    const parameters = {
+      count: 144
+    }
+
+    let color = {}
+    let particles = null
+
+    const generateParticles = (position, radius) => {
+
+      particles = {}
+
+
+      // Geometry
+      particles.geometry = new THREE.IcosahedronGeometry(5, 34)
+
+
+      // Material
+      particles.material = new THREE.ShaderMaterial({
+        // transparent: true,
+        // vertexColors: true,
+        vertexShader: vertexParticleShader,
+        fragmentShader: fragmentParticleShader,
+        uniforms: {
+          uSize: new THREE.Uniform(0.4), //* renderer.getPixelRatio(8),
+          // uTime: new THREE.Uniform(0),
+          uResolution: new THREE.Uniform(new THREE.Vector2(this.width * this.pixelRatio, this.height * this.pixelRatio)),
+          uColorAlpha: new THREE.Uniform(new THREE.Color(color.colorAlpha)),
+          uColorBeta: new THREE.Uniform(new THREE.Color(color.colorBeta)),
+        },
+        // blending: THREE.AdditiveBlending,
+        // depthWrite: false,
+      })
+
+      // Points
+      particles.points = new THREE.Points(particles.geometry, particles.material)
+      // particles.points.frustumCulled = false
+      // particles.points.position.copy(position).multiplyScalar(5)
+      // particles.points.position.x = 5
+      // particles.points.position.y = 3
+      // particles.points.position.z = 8
+
+      this.scene.add(particles.points)
+      // console.log(particles.points)
+
+    }
+
+
+    generateParticles(
+      new THREE.Vector3(),        // Position (Spherical)
+      144                          // Radius
+    )
+
+    // Setup 
+    this.environment = new Environment()
   }
 }

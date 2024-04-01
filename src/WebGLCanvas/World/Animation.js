e182292239c4e186f0cc560e6246ae3e1c909da2
p5eudoDjnn <alexander.havers@gmail.com>
2024-04-01 15:13:48 -0400

    animation added to the scene in a basic way

--- src/WebGLCanvas/World/Animation.js
+++ src/WebGLCanvas/World/Animation.js
@@ -1,8 +1,29 @@
 import * as THREE from 'three'
 import WebGLCanvas from '../WebGLCanvas'
 
+import animationVertexShader from '../../shaders/animation/vertexAnimation.glsl'
+import animationFragmentShader from '../../shaders/animation/fragmentAnimation.glsl'
+
 export default class Animation {
   constructor() {
-    console.log('this is for the animation')
+
+    this.webglCanvas = new WebGLCanvas()
+    this.scene = this.webglCanvas.scene
+    this.time = this.webglCanvas.time
+
+
+    // console.log('this is for the animation')
+
+    this.geometry = new THREE.IcosahedronGeometry(13, 2)
+    this.geometry.setIndex(null)
+    this.geometry.deleteAttribute('normal')
+
+    this.material = new THREE.ShaderMaterial({
+      vertexShader: animationVertexShader,
+      fragmentShader: animationFragmentShader,
+    })
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material)
+    this.scene.add(this.mesh)
   }
 }

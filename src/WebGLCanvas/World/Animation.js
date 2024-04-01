8c33006295dcc66c1798199b88c1db6788a34b2b
p5eudoDjnn <alexander.havers@gmail.com>
2024-04-01 15:27:16 -0400

    made the animation conversion without any time as of now

--- src/WebGLCanvas/World/Animation.js
+++ src/WebGLCanvas/World/Animation.js
@@ -18,9 +18,41 @@ export default class Animation {
     this.geometry.setIndex(null)
     this.geometry.deleteAttribute('normal')
 
+    this.count = this.geometry.attributes.position.count
+    this.randomizer = new Float32Array(this.count)
+
+    for (let i = 0; i < this.count; i++) {
+      this.randomizer[i] = Math.floor(this.count * Math.random() * -2 - 1)
+    }
+
+    this.geometry.setAttribute('aRandom', new THREE.BufferAttribute(this.randomizer, 3))
+
+    this.materialAnimationParamters = {}
+    this.materialAnimationParamters.color = '#181818'
+
     this.material = new THREE.ShaderMaterial({
+      // wireframe: true,
       vertexShader: animationVertexShader,
       fragmentShader: animationFragmentShader,
+      transparent: true,
+      side: THREE.DoubleSide,
+      uniforms: {
+        //
+        uColor: new THREE.Uniform(new THREE.Color(this.materialAnimationParamters.color)),
+        uColorOffset: new THREE.Uniform(0.925),
+        uColorMultiplier: new THREE.Uniform(1),
+        //
+        uFrequency: new THREE.Uniform(new THREE.Vector2(13, 8)),
+        uResolution: new THREE.Uniform(new THREE.Vector2(this.width * this.pixelRatio, this.height * this.pixelRatio)),
+        uTimeAnimation: new THREE.Uniform(0),
+        uTime: new THREE.Uniform(0),
+        //
+        uWaveElevation: new THREE.Uniform(0.8),
+        uWaveFrequency: new THREE.Uniform(new THREE.Vector2(13, 3.5)),
+        uWaveSpeed: new THREE.Uniform(0.89),
+      },
+      depthWrite: false,
+      blending: THREE.AdditiveBlending,
     })
 
     this.mesh = new THREE.Mesh(this.geometry, this.material)

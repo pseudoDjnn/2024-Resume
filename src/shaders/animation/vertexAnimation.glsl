4cdbdb2aeb523d16c39b97c3e51f7925fc56d7ee
p5eudoDjnn <alexander.havers@gmail.com>
2024-04-13 23:55:21 -0400

    added audio to the rational in use to make some noise angled

--- src/shaders/animation/vertexAnimation.glsl
+++ src/shaders/animation/vertexAnimation.glsl
@@ -1,19 +1,19 @@
 #define PI 3.1415926535897932384626433832795
 
-uniform vec2 uFrequency;
-uniform vec2 uResolution;
-uniform vec2 uWaveFrequency;
+// uniform vec2 uFrequency;
+// uniform vec2 uResolution;
+// uniform vec2 uWaveFrequency;
 
 uniform float uAudioFrequency;
 uniform float uTimeAnimation;
 uniform float uTime;
-uniform float uWaveElevation;
-uniform float uWaveSpeed;
+// uniform float uWaveElevation;
+// uniform float uWaveSpeed;
 
 attribute float aRandom;
 
 varying float vRandom;
-varying float vElevation;
+// varying float vElevation;
 varying vec3 vNormal;
 varying vec3 vPosition;
 varying vec2 vUv;
@@ -21,42 +21,25 @@ varying vec2 vUv;
 // #include ../includes/effects/perlin.glsl
 #include ../includes/effects/random2D.glsl
 #include ../includes/effects/terrainGeneration.glsl
-// #include ../includes/effects/simplexNoise3D.glsl
-
-float cubicPolynomial(float x) {
-  return x * x * (3.0 - 2.0 * x);
-}
+#include ../includes/effects/boxFrame.glsl
 
 float quarticPolynomial(float x) {
   return x * x * (2.0 - x * x);
 }
 
-float quinticPolynomial(float x) {
-  return x * x * x * (x * (x * 6.0 - 15.0) + 10.0);
-}
-
 float quadraticRational(float x) {
-  return x * x / (2.0 * x * x - 2.0 * x + 1.0);
-}
-
-float cubicRational(float x) {
-  return x * x * x / (3.0 * x * x - 3.0 * x + 1.0);
-}
-
-float rational(float x, float n) {
-  return pow(x, n) / (pow(x, n) + pow(1.0 - x, n));
-}
-
-float piecewiseQuadratic(float x) {
-  return (x < 0.5) ? 2.0 * x * x : 2.0 * x * (2.0 - x) - 1.0;
-}
-
-float piecewisePolynomial(float x, float n) {
-  return (x < 0.5) ? 0.5 * pow(2.0 * x, n) : 1.0 - 0.5 * pow(2.0 * (1.0 - x), n);
+  return x * x / (2.0 * x * x - uAudioFrequency * 0.05 * x + 1.0);
 }
 
 float trigonmetric(float x) {
   return 0.5 - 0.5 * cos(PI * x);
+}
+
+// quadratic polynomial
+float quadraticPolynomial(float a, float b, float k) {
+  k *= 4.0;
+  float h = max(k - abs(a - b), 0.0) / k;
+  return min(a, b) - h * h * k * (1.0 / 4.0);
 }
 
 void main() {
@@ -67,14 +50,14 @@ void main() {
   vec3 modelPositionBeta = modelPosition.xyz + vec3(0.0, 0.0, -shift);
 
   float elevation = terrainGeneration(modelPosition.xyz, 0.0);
-  // float frame = sdBoxFrame(modelPositionAlpha, modelPositionBeta, elevation);
+  vec3 frame = calcNormal(vec3(elevation));
   // elevation = pow(elevation, 2.0);
   // elevation += smoothstep(0.3, 1.0, uAudioFrequency);
   // modelPosition.x += sin(abs(uTimeAnimation * ceil(floor(PI * fract(uAudioFrequency * 0.02)) * 2.0 + 1.0)));
-  modelPosition.z += elevation;
+  // modelPosition.z = frame / 1.0;
 
-  // modelPosition.z += quarticPolynomial(elevation);
-  // modelPosition.y += quadraticRational(elevation) * 0.2;
+  // modelPosition.z += quadraticPolynomial(uTime, 1.0, -uTime);
+  modelPosition.yx += quadraticRational(elevation) * 0.001;
   // modelPosition.z += trigonmetric(elevation) * 0.2;
 
   // modelPosition.x -= sin(uAudioFrequency * 0.02 * ceil(floor(PI * fract(elevation * 0.0002)) * 2.0 + 1.0));
@@ -93,9 +76,12 @@ void main() {
   vec3 alphaNeighbor = normalize(modelPositionAlpha - modelPosition.xyz);
   vec3 betaNeighbor = normalize(modelPositionBeta - modelPosition.xyz);
 
+  // float boxFrame = sdBoxFrame(alphaNeighbor, betaNeighbor, elevation);
+
   // float boxFrame = sdRoundBox(alphaNeighbor, betaNeighbor, 1.0);
   // modelPosition.z += boxFrame;
   vec3 computeNormal = cross(alphaNeighbor, betaNeighbor);
+  // computeNormal *= frame;
   // modelPosition += map(vec3(computeNormal * 0.2));
 
   // computeNormal = smoothstep(0.8, 0.0, computeNormal);

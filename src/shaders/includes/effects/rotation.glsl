adc928848245b89a04f92a9ca5b49a8cb9138a8d
p5eud0Djinn <alexander.havers@gmail.com>
2025-02-11 12:43:49 -0500

    added uFrequencyData to the rotateZ function

--- src/shaders/includes/effects/rotation.glsl
+++ src/shaders/includes/effects/rotation.glsl
@@ -33,5 +33,5 @@ mat3 rotateY(float angle) {
 mat3 rotateZ(float angle) {
   float c = cos(angle);
   float s = sin(angle);
-  return mat3(c, -s, 0.0, s, c, 0.0, 0.0, uTime * 0.8, 1.0);
+  return mat3(c, -s, smoothstep(0.0, 1.0, uFrequencyData[144]), s, c, 0.0, 0.0, uTime * 0.8, 1.0);
 }
